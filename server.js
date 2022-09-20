0abf4758d634ce485abcfebea6f482e3f0677828
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-09-19 23:10:39 -0400

    server.js set up, no idea if it works

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const api = require('./routes/index.js');
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
